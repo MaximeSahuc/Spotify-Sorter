85c1497acb5d32a44241ff92a8c68b593ac37ad3
Maxime Sahuc <maxime.sahuc76@gmail.com>
2024-03-07 21:17:58 +0000

    Feature playlist searchbar

--- src/static/js/spotify.js
+++ src/static/js/spotify.js
@@ -4,47 +4,53 @@ var USER_ID;
 var AVAILIBLE_PLAYLISTS;
 
 async function getUserData() {
-    var response = await fetch('/me');
-    switch (response.status) {
-        case 200:
-            return await response.json();
-            break;
+  var response = await fetch("/me");
+  switch (response.status) {
+    case 200:
+      return await response.json();
+      break;
 
-        case 401:
-            window.location = '/login';
-            break;
+    case 401:
+      window.location = "/login";
+      break;
 
-        default:
-            console.error('Cannot login >:(');
-            document.getElementById('track-author').innerHTML = `${response.status}: Cannot login >:(`;
-            break;
-    }
+    default:
+      console.error("Cannot login >:(");
+      document.getElementById(
+        "track-author"
+      ).innerHTML = `${response.status}: Cannot login >:(`;
+      break;
+  }
 }
 
 async function getUserPlaylists() {
-    const response = await fetch('/playlists');
-    const data = await response.json();
+  const response = await fetch("/playlists");
+  const data = await response.json();
 
-    return data.playlists;
+  return data.playlists;
 }
 
 async function saveTrackToPlaylists(trackId, toPlaylists) {
-    const postData = {
-        track: trackId,
-        playlists: toPlaylists
-    };
+  const postData = {
+    track: trackId,
+    playlists: toPlaylists,
+  };
 
-    const response = await fetch('/playlists', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(postData)
-    });
+  const response = await fetch("/playlists", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(postData),
+  });
 
-    const status = await response.status;
-    if (status != 200)
-        console.log(`saving track status : ${status}`);
+  const status = await response.status;
+  if (status != 200) console.log(`saving track status : ${status}`);
 }
 
 async function loadUserPlaylists() {
-    AVAILIBLE_PLAYLISTS = await getUserPlaylists();
+  AVAILIBLE_PLAYLISTS = await getUserPlaylists();
+  addPlaylistsToPopup(0, AVAILIBLE_PLAYLISTS);
+}
+
+function searchPlaylists(query) {
+  return; // TODO
 }
