05bff1a72586b1a838b77815b5b2b61a270d5d8f
Maxime Sahuc <maxime.sahuc76@gmail.com>
2024-03-09 19:29:58 +0000

    Feature - config using env file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,1 @@
-module.exports = SPOTIFY_CLIENT_ID = '1eac066224d34a70b0f1dd6721f3b992';
-module.exports = SPOTIFY_CLIENT_SECRET = 'ea8388194e3c4413ad0b718037700bb1';
-
 module.exports = PORT = 3000;
